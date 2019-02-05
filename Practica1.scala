a2d1d0e1e932aac0f7c46b1407781e7dbbce6be9
juliovillalobos14 <julio.gamino@tectijuana.edu.mx>
2019-02-05 15:18:42 -0800

    Practica_Unidad1

--- Practica1.scala
+++ Practica1.scala
@@ -2,9 +2,19 @@
 
 object Practica1 {
   def main(args: Array[String]): Unit = {
-        println("Hola")
-        val Conversion_Metros1 = 3 / 3.28
-        println(Conversion_Metros1)
+        val Datos = List(3,10,15)
+        val met = 3.2808
+        val dll = 18.0
+        val mill = 0.62137
+        for(name <- Datos) {
+          println(name + " Pies es igual en metros a: " + (name / met))
+        }
+        for(name <- Datos) {
+          println(name + " Dollares a pesos son:  " + (name * dll))
+        }
+        for(name <- Datos) {
+          println(name + " Millas son en kilometros: " + (name * mill))
+        }
         
   }
  }

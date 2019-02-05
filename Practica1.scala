45ab37b5a54da1500c02365594de74e6b20af695
juliovillalobos14 <julio.gamino@tectijuana.edu.mx>
2019-02-05 13:01:09 -0800

    Actualizacion

---
+++ Practica1.scala
@@ -0,0 +1,11 @@
+object Practica1 {
+    def main(args: Array[String]): Unit = {
+    //Conversión de Pies A Metros
+        val Conversion_Metros1 = 3 / 3.28
+        val Conversion_Metros2 = 10 / 3.28
+        val Conversion_Metros3 = 15 / 3.28
+        printf("La conversión de los números 3, 10 y 15 a metros son: %s, %d y %f", Conversion_Metros1, Conversion_Metros2, Conversion_Metros3)
+        //Conversión de Dollares a Pesos
+        //Conversión de Millas a Kilometros
+    }
+}
